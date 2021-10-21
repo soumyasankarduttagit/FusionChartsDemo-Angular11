48e7ba8cb253124795fc4b9a69a43a73d5483f55
unknown <saymesomu@gmail.com>
2021-10-21 20:04:52 +0530

    FusionCharts sample in Angular 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FusionChartsDemo11';
+  dataSource: Object;
+  title: string;
+
+  constructor() {
+    this.title = 'Angular11  FusionCharts Sample';
+
+    this.dataSource = {
+      chart: {
+        caption: 'Countries With Most Oil Reserves [2017-18]',
+        subCaption: 'In MMbbl = One Million barrels',
+        xAxisName: 'Country',
+        yAxisName: 'Reserves (MMbbl)',
+        numberSuffix: 'K',
+        theme: 'fusion'
+      },
+      data: [
+        { label: 'Venezuela', value: '290' },
+        { label: 'Saudi', value: '260' },
+        { label: 'Canada', value: '180' },
+        { label: 'Iran', value: '140' },
+        { label: 'Russia', value: '115' },
+        { label: 'UAE', value: '100' },
+        { label: 'US', value: '30' },
+        { label: 'China', value: '30' }
+      ]
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
 
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+
+// For Powercharts , Widgets, and Maps
+// import * as PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+// import * as Widgets from 'fusioncharts/fusioncharts.widgets';
+// import * as Maps from 'fusioncharts/fusioncharts.maps';
+// To know more about suites,
+// read this https://www.fusioncharts.com/dev/getting-started/plain-javascript/install-using-plain-javascript
+
+// For Map definition files
+// import * as World from 'fusioncharts/maps/fusioncharts.world';
+
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
